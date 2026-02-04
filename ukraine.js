98e6717128204e32e92d0b0aeb4a59afbc39caa1
vitalik19970527-prog <vitalik.1997.05.27@gmail.com>
2026-02-04 19:33:18 +0200

    Update ukraine.js

--- ukraine.js
+++ ukraine.js
@@ -4,8 +4,8 @@
     if (window.Lampa && Lampa.Plugins) {
         Lampa.Plugins.add({
             name: 'Давай UA',
-            version: '1.3.0',
-            description: 'Пошук української озвучки',
+            version: '1.4.0',
+            description: 'Українська озвучка',
             type: 'video',
             author: 'Vitalik'
         });
@@ -21,16 +21,12 @@
             var html = $('<div class="directory-layers"></div>');
             
             this.create = function () {
-                var _this = this;
                 html.append(scroll.render());
                 var m = object.movie || {};
-                var title = m.title || m.name;
-                var year = (m.release_date || m.first_air_date || '').slice(0, 4);
-                var url = 'https://api.lampa.stream/mod?title=' + encodeURIComponent(title) + '&year=' + year;
+                var url = 'https://api.lampa.stream/mod?title=' + encodeURIComponent(m.title || m.name) + '&year=' + (m.release_date || m.first_air_date || '').slice(0, 4);
                 
                 network.silent(url, function (data) {
                     if (data && data.length) {
-                        var count = 0;
                         data.forEach(function(item) {
                             var t = (item.title || '').toLowerCase();
                             if (item.file && (t.indexOf('ua') > -1 || t.indexOf('україн') > -1)) {
@@ -39,42 +35,46 @@
                                     Lampa.Player.play({ url: item.file, title: item.title }); 
                                 });
                                 scroll.append(card);
-                                count++;
                             }
                         });
-                        if (count === 0) Lampa.Noty.show('Української озвучки не знайдено');
                     } else { Lampa.Noty.show('Нічого не знайдено'); }
                 });
             };
             this.render = function () { return html; };
         });
 
-        // ПРИМУСОВА ВСТАВКА КНОПКИ
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite' || e.type == 'ready') {
                 var render = function() {
-                    // Шукаємо будь-яке місце для кнопки: блок кнопок або просто під опис
-                    var parent = e.object.container.find('.full-start__buttons, .full-start, .full-info__content');
+                    // Використовуємо ваш новий клас full-start-new__buttons
+                    var container = e.object.container.find('.full-start-new__buttons');
                     
-                    if (parent.length && !e.object.container.find('.dvua-btn').length) {
-                        var btn = $('<div class="full-start__button selector dvua-btn" style="background: #243b55; border: 1px solid #00c6ff; margin-top: 10px;"><span>🇺🇦 Давай UA</span></div>');
+                    if (container.length && !container.find('.dvua-btn').length) {
+                        var btn = $(`
+                            <div class="full-start__button selector dvua-btn">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="2"/>
+                                    <path d="M12 8V16M8 12H16" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+                                </svg>
+                                <span>Давай UA</span>
+                            </div>
+                        `);
                         
                         btn.on('hover:enter', function () { 
                             Lampa.Controller.push('davay_ua', { movie: e.data.movie }); 
                         });
 
-                        // Вставляємо в початок блоку кнопок або в кінець контенту
-                        if (parent.hasClass('full-start__buttons')) parent.prepend(btn);
-                        else parent.append(btn);
+                        // Вставляємо після кнопки "Дивитись"
+                        var playBtn = container.find('.button--play');
+                        if (playBtn.length) playBtn.after(btn);
+                        else container.prepend(btn);
                         
-                        // Оновлення навігації
+                        // Оновлення фокусу
                         Lampa.Controller.toggle('full_start');
                     }
                 };
 
-                // Пробуємо кілька разів, бо інтерфейс може довантажуватись
                 setTimeout(render, 500);
-                setTimeout(render, 1500);
             }
         });
     }
@@ -82,10 +82,7 @@
     if (window.Lampa && Lampa.Component) init();
     else {
         var timer = setInterval(function () {
-            if (window.Lampa && Lampa.Component) {
-                clearInterval(timer);
-                init();
-            }
+            if (window.Lampa && Lampa.Component) { clearInterval(timer); init(); }
         }, 500);
     }
 })();
